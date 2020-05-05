bdfa12f559764c30d3051cd77fb95c4677537d65
ljaviertovar <luisjavier_tovar@outlook.com>
2020-05-05 09:16:13 -0500

    Feat: Add API Doc swagger

--- api/index.js
+++ api/index.js
@@ -1,15 +1,20 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 
+const swaggerUi = require('swagger-ui-express')
+;
 const config =  require('../config.js');
 const user = require('./components/user/network.js');
 
 const app = express();
 
+const swaggerDoc = require('./swagger.json');
+
 app.use(bodyParser.json());
 
 // ROUTER 
 app.use('/api/user', user);
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDoc));
 
 app.listen(config.api.port, () => {
     console.log('api listening...');
